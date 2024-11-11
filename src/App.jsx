e39bf7637bdd1f91808dae8bf34f1652e90d7644
sanjoy <sanjoylab66@gmail.com>
2024-11-11 22:08:17 +0600

    read student data and show

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
+import {useEffect, useState} from 'react'
 import './App.css'
+import { baseUrl } from './apis/apis'
+import axios from 'axios'
 
 export default function App() {
 
+  const [studentsData, setStudentsData] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${baseUrl}/students`)
+    .then(res => res.data)
+    .then(data => setStudentsData(data))
+    .catch(error => console.log(error))
+  }, [])
+  
+
   return (
-    <div>
-      App
-    </div>
+    <>
+      {/* Student List */}
+      <div className='studentsDiv'>
+        {
+          studentsData?.map((data,index)=>{
+            return(
+              <div key={index}> 
+                <p>{data.name}</p>
+                <p>{data.roll}</p>
+              </div>
+            )
+          })
+        }
+      </div>
+    </>
   )
 }
